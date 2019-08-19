efbd588d9312f30ef352d6fd2b53da7b948d2a6f
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-08-19 20:18:54 +1000

    Refactor password program to use functions.

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -2,10 +2,23 @@
 Katrina McKeough
 """
 
-min_password_length = 4
-password = input("Please enter a password (minimum {} characters): ".format(min_password_length))
 
-while len(password) < min_password_length:
-    print("Password too short!")
+def main():
+    min_password_length = 4
+    password = get_password(min_password_length)
+    print_asterisks(password)
+
+
+def get_password(min_password_length):
     password = input("Please enter a password (minimum {} characters): ".format(min_password_length))
-print('*' * len(password))
+    while len(password) < min_password_length:
+        print("Password too short!")
+        password = input("Please enter a password (minimum {} characters): ".format(min_password_length))
+    return password
+
+
+def print_asterisks(password):
+    print('*' * len(password))
+
+
+main()
