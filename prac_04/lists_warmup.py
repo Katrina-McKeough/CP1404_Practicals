40a1b4cac1a7dff9548816cc49e7bbd6e705eb27
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-08-27 22:18:53 +1000

    Provide answers to list warmup questions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,24 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Questions:
+# numbers[0] will return 3
+# numbers[-1] will return 2
+# numbers[3] will return 1
+# numbers[:-1] will return [3, 1, 4, 1, 5, 9]
+# numbers[3:4] will return [1]
+# 5 in numbers will return True
+# 7 in numbers will return False
+# "3" in numbers will return False
+# numbers + [6, 5, 3] will return [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+#
+# Remove from comments to see if correct:
+# print(numbers[0])
+# print(numbers[-1])
+# print(numbers[3])
+# print(numbers[:-1])
+# print(numbers[3:4])
+# print(5 in numbers)
+# print(7 in numbers)
+# print("3" in numbers)
+# print(numbers + [6, 5, 3])
+
