85b3b1493e42b7eb7087bf07ceea60348f881662
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-08-28 13:11:04 +1000

    Add woefully inadequate security checker program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -10,3 +10,10 @@ print("The largest number is {}".format(max(numbers)))
 print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
 
 """ Woefully Inadequate Security Checker """
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Please enter your username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
