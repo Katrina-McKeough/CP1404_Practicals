52054f758aba8d97c6a1bc3b73a515d70cd7b06c
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-08-28 13:26:34 +1000

    Remove redundant code in list exercises program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,7 +2,6 @@
 numbers = []
 for i in range(5):
     numbers.append(int(input("Number: ")))
-    i += 1
 print("The first number is {}".format(numbers[0]))
 print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min(numbers)))
