0f284831ef31d640df048ebe1d946a9bc09883e0
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-09 15:45:03 +1000

    Add code that prints quick picks and prevents numbers from repeating

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,8 +7,14 @@ MAX_VALUE = 45
 
 number_of_picks = int(input("How many quick picks? "))
 for i in range(number_of_picks):
-    numbers_in_line = []
+    quick_pick = []
     for j in range(LINE_LENGTH):
-        numbers_in_line.append(random.randint(MIN_VALUE, MAX_VALUE))
-        numbers_in_line.sort()
-# TODO write code that prints quick picks
+        number = random.randint(MIN_VALUE, MAX_VALUE)
+        while number in quick_pick:
+            number = random.randint(MIN_VALUE, MAX_VALUE)
+        quick_pick.append(number)
+    quick_pick.sort()
+    formatted_quick_pick = []
+    for number in quick_pick:
+        formatted_quick_pick.append("{:2d}".format(number))
+    print(" ".join(formatted_quick_pick))
