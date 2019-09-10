6d5e969deb05de072f83b20d1278cccfbf61d27f
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-10 21:33:19 +1000

    Add hex_colours.py containing a hexadecimal colour codes dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+COLOUR_CODES = {"AliceBlue": "#fof8ff", "CadetBlue1": "#98f5ff", "CornflowerBlue": "#6495ed",
+                "DarkTurquoise": "#00ced1", "DeepSkyBlue1": "#00bfff", "DodgerBlue1": "#1e90ff", "LightBlue": "#add8e6",
+                "LightCyan1": "#e0ffff", "LightSkyBlue": "#87cefa", "MidnightBlue": "#191970"}
