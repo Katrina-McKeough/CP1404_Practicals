b39f844cc03a279516d51ea09af3445ffa3d9a73
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-11 12:19:22 +1000

    Improve variable names

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,10 +1,10 @@
 text = input("Text: ")
 words = text.split(" ")
 max_length = max(len(word) for word in words)
-word_occurrences = {}
+words_to_occurrences = {}
 for word in words:
-    word_occurrences[word] = word_occurrences.get(word, 0) + 1
-words = list(word_occurrences.keys())
+    words_to_occurrences[word] = words_to_occurrences.get(word, 0) + 1
+words = list(words_to_occurrences.keys())
 words.sort()
 for word in words:
-    print("{:{}s} : {}".format(word, max_length, word_occurrences[word]))
+    print("{:{}s} : {}".format(word, max_length, words_to_occurrences[word]))
