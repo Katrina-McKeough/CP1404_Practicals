0ff85cc403b1094ba8b94c000e020f3dd56f1cde
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-23 22:35:57 +1000

    Add programming_language.py and define __init__ method

---
+++ prac_06/programming_language.py
@@ -0,0 +1,17 @@
+
+class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage object."""
+
+    def __init__(self, name, typing, reflection, year):
+
+        """Initialise a ProgrammingLanguage instance.
+
+        name: string
+        typing: string
+        reflection: boolean
+        year: integer
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
