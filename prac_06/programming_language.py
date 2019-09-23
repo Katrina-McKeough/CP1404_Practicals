c05d19362d682ac44410682c948cd19962175034
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-23 22:56:17 +1000

    Improve formatting and add __str__ method to ProgrammingLanguage class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,9 +1,7 @@
-
 class ProgrammingLanguage:
     """Represent a ProgrammingLanguage object."""
 
     def __init__(self, name, typing, reflection, year):
-
         """Initialise a ProgrammingLanguage instance.
 
         name: string
@@ -18,3 +16,8 @@ class ProgrammingLanguage:
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Convert the instance to a string."""
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection,
+                                                                            self.year)
