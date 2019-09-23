0c1c586e11799b5485084785db363d92015851e0
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-23 23:14:36 +1000

    Define __str__ method for Guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,4 +1,3 @@
-
 class Guitar:
     """Represent a Guitar object."""
 
@@ -8,6 +7,10 @@ class Guitar:
         year: integer
         cost: float
         """
+
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
