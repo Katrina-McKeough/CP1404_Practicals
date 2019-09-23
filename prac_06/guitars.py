4b831ba584e7055e26f7d07aed6029e36b3e52df
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-24 00:06:09 +1000

    Add code to list details of user's guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -14,3 +14,11 @@ while not name == "":
 # Test code:
 # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+print(". . . snip . . .")
+
+print("These are my guitars:")
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = "(vintage)" if guitar.is_vintage() is True else ""
+    print(
+        "Guitar {}: {:>20} ({}), worth ${:10,.2f} {}".format(i, guitar.name, guitar.year, guitar.cost, vintage_string))
