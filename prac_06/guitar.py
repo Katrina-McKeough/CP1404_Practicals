b4395dcd25cc728f7ffd4525f4d9519f0cc8a50e
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-23 23:08:45 +1000

    Add guitar.py and define __init__ method for Guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,13 @@
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance.
+        name: string
+        year: integer
+        cost: float
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
