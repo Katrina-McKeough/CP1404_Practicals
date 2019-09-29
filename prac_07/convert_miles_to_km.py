20f2f78f23507a03c579a37a0bcb70f2027e0f52
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-09-29 23:38:03 +1000

    Modify code to make use of StringProperty() to display output label text
    Signed-off-by: katrina-mckeough <katrina.mckeough@jcu.edu.au>

--- prac_07/convert_miles_to_km.py
+++ prac_07/convert_miles_to_km.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.app import StringProperty
 
 MILES_TO_KM_CONVERSION = 1.60934  # Conversion factor is 1.60934 km/ 1 mile
 
 
 class ConvertMilesApp(App):
     """ ConvertMilesApp is a Kivy App for converting miles to kilometres from user input """
+
+    output_text = StringProperty()
 
     def build(self):
         """ build the Kivy app from the kv file """
@@ -18,7 +21,7 @@ class ConvertMilesApp(App):
     def handle_calculate(self, value):
         """ handle calculation from button press, output result to label widget"""
         result = value * MILES_TO_KM_CONVERSION
-        self.root.ids.output_km.text = str(result)
+        self.output_text = str(result)
 
 
 ConvertMilesApp().run()
