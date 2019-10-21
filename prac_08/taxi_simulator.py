b698dc7eeba1803f5019ee13802ae4d77238674e
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-10-21 22:10:15 +1000

    Refactor display taxi code into a function
    Signed-off-by: katrina-mckeough <katrina.mckeough@jcu.edu.au>

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -8,21 +8,30 @@ def main():
     """ Taxi simulator program that allows users to choose from available taxis for a trip. """
 
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    total_cost = 0
 
+    print("Let's drive!")
     print(MENU)
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
             print("Taxis available:")
-            for i, taxi in enumerate(taxis):
-                print("{} - {}".format(i, taxi))
+            display_taxis(taxis)
+            chosen_taxi = input("Choose taxi: ")
         elif choice == "d":
             print("")
         else:
             print("Invalid option")
+        print("Bill to date: ${:2f}".format(total_cost))
         print(MENU)
         choice = input(">>> ").lower()
-    print("Total trip cost: ")
+    print("Total trip cost: ${:2f}".format(total_cost))
+    print("Taxis are now:")
+
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, taxi))
 
 
 main()
