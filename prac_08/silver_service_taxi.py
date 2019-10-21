3e01a8036d57aeebded3073171b755dae3b0b105
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-10-21 22:51:10 +1000

    Add docstrings to files
    Signed-off-by: katrina-mckeough <katrina.mckeough@jcu.edu.au>

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -1,3 +1,7 @@
+"""
+CP1404/CP5632 Practical
+SilverServiceTaxi class
+"""
 from prac_08.taxi import Taxi
 
 
--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,3 +1,7 @@
+"""
+CP1404/CP5632 Practical
+Taxi simulator program
+"""
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
