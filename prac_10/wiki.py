de66fecfa4d87a3f80d4f97d9fd03aba0c11df2c
katrina-mckeough <katrina.mckeough@jcu.edu.au>
2019-10-27 02:21:00 +1000

    Modify wiki.py to print page title, summary and URL
    Signed-off-by: katrina-mckeough <katrina.mckeough@jcu.edu.au>

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,9 +1,13 @@
 import wikipedia
 
+summary = ""
 search_phrase = input("Enter a page title or phrase to search: ")
 while search_phrase:
     try:
-        print(wikipedia.summary(search_phrase))
+        page = wikipedia.page(search_phrase)
+        print(page.title)
+        print(page.summary)
+        print(page.url)
     except wikipedia.exceptions.DisambiguationError as e:
         print("Try searching one of: {}".format(e.options))
     except wikipedia.exceptions.PageError:
